e6101c28924cf1ec52f78b4fa973e7f431278054
Tejugitacc <thejashwini.ravikumar@relevancelab.com>
2025-12-08 17:47:14 +0530

    removed console

--- src/helpers/authHeader.js
+++ src/helpers/authHeader.js
@@ -1,9 +1,6 @@
 import { WEBAPI_Key } from "../constants/webApiKey.js";
 
 export async function authHeader() {
-  console.log("WEBAPI_Key used in authHeader:", WEBAPI_Key);
-
-   console.log("RAW VALUE:", JSON.stringify(WEBAPI_Key));
   return {
     "Appian-API-Key": WEBAPI_Key,
     "Accept": "application/json",
