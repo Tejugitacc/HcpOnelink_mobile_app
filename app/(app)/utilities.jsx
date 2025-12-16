fe62bb3d290828c488c9ed4bb7df358a306c9754
Tejugitacc <thejashwini.ravikumar@relevancelab.com>
2025-12-16 13:41:21 +0530

    font signature added

--- app/(app)/utilities.jsx
+++ app/(app)/utilities.jsx
@@ -1,5 +1,6 @@
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Crypto from "expo-crypto";
+import { useFonts } from "expo-font";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
@@ -13,6 +14,14 @@ export default function Utilities() {
   const [date, setDate] = useState(new Date());
   const [bankNumber, setBankNumber] = useState("");
   const [encrypted, setEncrypted] = useState("");
+  const [signName, setSignName] = useState("");
+
+
+  const [fontsLoaded] = useFonts({
+    SignatureFont: require("../../assets/fonts//Pacifico-Regular.ttf")
+  });
+
+  if (!fontsLoaded) return null;
 
   // 📸 1. Capture Image using Camera
   const openCamera = async () => {
@@ -68,7 +77,7 @@ export default function Utilities() {
       </TouchableOpacity>
       {file && <Text style={styles.fileText}>Selected: {file.fileName || "File"}</Text>}
 
-      {/* SIGNATURE */}
+      {/* SIGNATURE CANVAS*/}
       <Text style={styles.section}>Signature</Text>
       <Signature
         onOK={handleSignature}
@@ -76,6 +85,23 @@ export default function Utilities() {
         backgroundColor="#f8f8f8"
       />
       {signature && <Text style={styles.success}>✔ Signature captured</Text>}
+
+
+      {/* SIGNATURE BY NAME */}
+      <Text style={styles.section}>Signature</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter full name"
+        value={signName}
+        onChangeText={setSignName}
+      />
+
+      {signName ? (
+        <View style={styles.signatureBox}>
+          <Text style={styles.signatureText}>{signName}</Text>
+        </View>
+      ) : null}
 
       {/* DATE PICKER */}
       <TouchableOpacity style={styles.btn} onPress={() => setShowPicker(true)}>
@@ -104,7 +130,7 @@ export default function Utilities() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 14, backgroundColor: "#fff", paddingTop: 50 },
-  title: { fontSize: 26, fontWeight: "bold", marginBottom: 10 },
+  title: { fontSize: 20, fontWeight: "bold", marginBottom: 10 },
   btn: {
     backgroundColor: "#1d6dce",
     padding: 12,
@@ -125,4 +151,19 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   hash: { fontSize: 12, color: "#333", marginTop: 5 },
+  signatureBox: {
+    marginTop: 10,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    backgroundColor: "#fafafa",
+  },
+
+  signatureText: {
+    fontFamily: "SignatureFont",
+    fontSize: 36,
+    color: "#000",
+  },
+
 });
